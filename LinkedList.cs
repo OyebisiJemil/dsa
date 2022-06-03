e456dea5577e9906ea6b2f88276859fa06f95ca8
Jemil <oyebisijemil@gmail.com>
2022-06-02 20:37:01 -0400

    Find start of a cycle Add solution to find the start of a cycle

--- LinkedList.cs
+++ LinkedList.cs
@@ -39,5 +39,56 @@ namespace dsa
 
             return false; // if we gets here, there is no cycle
         }
+
+
+        /*
+            1 ==> 2 ==> 3 ==> ==> 4 ==> 5 ==> 6
+                        ||                   ||
+                        ======================
+
+            Detect a cycle
+            ==============
+            Tortoise and hare started from head
+            Tortoise moves one step while the hare moves 2 steps, at some point, they'll both meet
+
+            Detect start of cycle
+            =====================
+            A new slow animal snail started its own jouney from head while tortoise continues its own jorney from point k
+            Tortoise and snail will end up meeting at the entry point of the cycle
+
+            Complexity
+            ==========
+            T(n) ==> O(n)
+            S(n) ==> O(1)
+        */
+        ListNode StartOfCycle(ListNode head)
+        {
+            if(head == null) //edge case
+            return null;
+
+            var tortoise = head;
+            var hare = head;
+
+            while(hare.next != null && hare.next.next != null)
+            {
+                //move both forward
+                hare = hare.next.next;
+                tortoise = tortoise.next;
+
+                //detect cycle
+                if(slow == hare)
+                {
+                    //find where the cycle begins
+                    var snail = head;
+                    while(tortoise != snail)
+                    {
+                        tortoise = tortoise.next;
+                        snail = snail.next;
+                    }
+                    return snail;
+                }
+            }
+            return null; //return null if there is not cycle
+        }
     }
 }
